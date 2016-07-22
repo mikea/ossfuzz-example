d3208f3d9bdea4a3e5b678685229e95d8c09f46a
Mike Aizatsky <aizatsky@google.com>
2016-07-22 13:25:12 -0700

    updating to latest infra changes

--- fuzz/build.sh
+++ fuzz/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -ex
-. /work/llvm/env
+. /env
 
-./cmake -G "Ninja" \
+mkdir /work/ossfuzz-example
+cd /work/ossfuzz-example
+
+cmake -G "Ninja" \
   -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX \
-  -DCMAKE_C_FLAGS="$CFLAGS" -DCMAKE_CXX_FLAGS="$CXXFLAGS"
+  -DCMAKE_C_FLAGS="$CFLAGS" -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
+  /workspace
+
 ninja
