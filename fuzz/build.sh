e05505bfb86ab62718fa598041dae40c78723ce6
Mike Aizatsky <aizatsky@google.com>
2016-08-01 14:24:19 -0700

    updating to recent infra changes: removed workspace

--- fuzz/build.sh
+++ fuzz/build.sh
@@ -7,8 +7,8 @@ cd /work/ossfuzz-example
 cmake -G "Ninja" \
   -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX \
   -DCMAKE_C_FLAGS="$CFLAGS" -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
-  /workspace
+  /src/ossfuzz-example
 ninja
 
-$CXX $CXXFLAGS $LDFLAGS /workspace/fuzz/fuzzer.cc -o /out/fuzzer \
+$CXX $CXXFLAGS $LDFLAGS /src/ossfuzz-example/fuzz/fuzzer.cc -o /out/ossfuzzexample_fuzzer \
   /work/libfuzzer/*.o ./libexample.a
