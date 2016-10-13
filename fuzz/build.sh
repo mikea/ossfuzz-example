008e787047e432a7216b33871c44fc8c6137bcf8
Mike Aizatsky <mike.aizatsky@gmail.com>
2016-10-13 15:40:38 -0700

    Update build.sh

--- fuzz/build.sh
+++ fuzz/build.sh
@@ -5,9 +5,9 @@ cd /work/ossfuzz-example
 cmake -G "Ninja" \
   -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX \
   -DCMAKE_C_FLAGS="$CFLAGS" -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
-  -DCMAKE_EXE_LINKER_FLAGS="$LDFLAGS" \
+  -DCMAKE_EXE_LINKER_FLAGS="$FUZZER_LDFLAGS" \
   /src/ossfuzz-example
 ninja
 
-$CXX $CXXFLAGS $LDFLAGS /src/ossfuzz-example/fuzz/fuzzer.cc -o /out/ossfuzzexample_fuzzer \
+$CXX $CXXFLAGS $FUZZER_LDFLAGS /src/ossfuzz-example/fuzz/fuzzer.cc -o /out/ossfuzzexample_fuzzer \
   /work/libfuzzer/*.o ./libexample.a
