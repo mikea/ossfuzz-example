e960031d42c1a674c5237b7a2ecc3a72ac9ddd7e
Mike Aizatsky <mike.aizatsky@gmail.com>
2016-11-18 13:10:00 -0800

    FUZZER_LDFLAGS is no longer necessary

--- fuzz/build.sh
+++ fuzz/build.sh
@@ -5,9 +5,8 @@ cd /work/ossfuzz-example
 cmake -G "Ninja" \
   -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX \
   -DCMAKE_C_FLAGS="$CFLAGS" -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
-  -DCMAKE_EXE_LINKER_FLAGS="$FUZZER_LDFLAGS" \
   /src/ossfuzz-example
 ninja
 
-$CXX $CXXFLAGS $FUZZER_LDFLAGS /src/ossfuzz-example/fuzz/fuzzer.cc -o /out/ossfuzzexample_fuzzer \
+$CXX $CXXFLAGS /src/ossfuzz-example/fuzz/fuzzer.cc -o /out/ossfuzzexample_fuzzer \
   -lfuzzer ./libexample.a
